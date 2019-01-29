c4acf8f04bc75649644837874a6da542aef59b77
KMSkelton <kmskelton@outlook.com>
2019-01-28 23:46:27 -0800

    basic minesweeper based on coding train challenge #71

---
+++ cell.js
@@ -0,0 +1,85 @@
+function Cell(i, j, w) {
+  this.i = i
+  this.j = j
+  this.x = i*w
+  this.y = j*w
+  this.w = w
+  this.orcCount = 0
+
+  this.orc = false
+  // if(random(1) < 0.5){
+  //   this.orc = true
+  // } else {
+  //   this.orc = false
+  // }
+  this.revealed = false;
+}
+
+Cell.prototype.countOrcs = function() {
+  if (this.orc) {
+    this.orcCount = -1
+    return;
+  }
+  let total = 0
+  for (let xoff = -1; xoff <= 1; xoff++) {
+    for (let yoff = -1; yoff <= 1; yoff++) {
+      let i = this.i + xoff
+      let j = this.j + yoff
+      if ( i > -1 && i < cols && j > -1 && j < rows) {
+        let neighbor = grid[i][j]
+        if (neighbor.orc) {
+          total++
+        }
+      }
+    }
+  }
+  this.orcCount = total
+}
+
+Cell.prototype.show = function() {
+  stroke(0)
+  noFill();
+  rect(this.x, this.y, this.w, this.w)
+  if (this.revealed) {
+    if (this.orc) {
+      fill('rgba(1, 104, 4, 1)')
+      ellipseMode(CENTER)
+      ellipse(this.x+this.w*0.5, this.y+this.w*0.5, this.w*0.5)
+    } else {
+      fill(127)
+      rect(this.x, this.y, this.w, this.w)
+      if (this.orcCount > 0) {
+        textAlign(CENTER)
+        fill(0)
+        text(this.orcCount, this.x+this.w*0.5, this.y+this.w*.75)
+      }
+    }
+  }
+}
+
+Cell.prototype.contains = function(x, y) {
+  return (x > this.x && x < this.x + this.w
+    && y > this.y && y < this.y + this.w)
+}
+
+Cell.prototype.reveal = function(x, y) {
+  this.revealed = true
+  if (this.orcCount == 0) {
+    this.floodFill();
+  }
+}
+
+Cell.prototype.floodFill = function() {
+  for (let xoff = -1; xoff <= 1; xoff++) {
+    for (let yoff = -1; yoff <= 1; yoff++) {
+      let i = this.i + xoff
+      let j = this.j + yoff
+      if ( i > -1 && i < cols && j > -1 && j < rows) {
+        let neighbor = grid[i][j]
+        if (!neighbor.orc && !neighbor.revealed) {
+          neighbor.reveal();
+        } 
+      }
+    }
+  }
+}
---
+++ minesweeper.js
@@ -0,0 +1,86 @@
+let grid;
+let cols;
+let rows;
+let w = 20;
+
+let totalOrcs = 10
+
+function make2DArray(cols, rows) {
+  let arr = new Array(cols)
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr
+}
+
+
+function setup() {
+  createCanvas(201, 201)
+  cols = floor(width / w)
+  rows = floor(height / w)
+  grid = make2DArray(40, 40)
+  // make game board:
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      // this is to calc the pixel locations
+      grid[i][j] = new Cell(i, j, w);
+    }
+  }
+
+  // pick total orc spots
+  let options = []
+  for (let i = 0; i < cols; i++){
+    for(let j = 0; j < rows; j++){
+      options.push([i, j])
+    }
+  }
+  for (let n = 0; n < totalOrcs; n++){
+    const index = floor(random(options.length))
+    console.log(' :',index);
+    const choice = options[index]
+    const i = choice[0]
+    const j = choice[1]
+    options.splice(index, 1)
+    grid[i][j].orc = true
+  }
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      // this is to calc the pixel locations
+      grid[i][j].countOrcs();
+    }
+  }
+
+}
+
+function gameOver() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].revealed = true;
+    }
+  }
+  alert("Game over")
+}
+
+function mousePressed() {
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      if(grid[i][j].contains(mouseX, mouseY)){
+        grid[i][j].reveal()
+        if(grid[i][j].orc){
+          gameOver();
+        }
+      }
+    }
+  }
+}
+
+function draw() {
+  background(255);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].show();
+    }
+  }
+
+}
